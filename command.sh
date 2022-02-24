a18bc37522c7f2b0995d28c8184aae399457b7a3
Naveen Kumar Selvaraj <naveenkumars@parkisolutions.com>
2022-02-24 22:47:58 +0530

    command

---
+++ command.sh
@@ -0,0 +1,53 @@
+#to create a file
+touch abc.txt
+touch def.txt
+touch ghi.txt
+touch jkl.txt
+
+#list the file in directry
+ls -l
+
+#create a folder
+mkdir basename
+
+#moveing command
+mv abc.txt basename
+mv def.txt basename
+mv ghi.txt basename
+mv jkl.txt basename
+
+#creating files with name abc.log.1.............
+touch abc.log.1
+touch def.log.1
+touch ghi.log.1
+touch jkl.log.1
+touch mno.log.1
+
+#list of file name
+ls -l
+ls -a
+ls -r
+
+#date command
+date
+
+#exit command
+mkdir file1
+cd file1
+cd ..
+echo "folder is exit"
+
+#environment
+env usersecret="dH34xjaa23"
+
+#find the file name
+find . - name systemd
+
+#memory storage
+du-h
+
+#cp command
+nano test
+nano pro
+cp test pro
+
